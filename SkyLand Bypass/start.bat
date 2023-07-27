5bf557e78ea4a6a23be1fd0c3cbdeb67a4501c1e
SkyLoLand777 <128433461+SkyLoLand777@users.noreply.github.com>
2023-07-27 15:39:52 -0300

    Update start.bat

--- SkyLand Bypass/start.bat
+++ SkyLand Bypass/start.bat
@@ -1,7 +1,7 @@
 @echo off
 title SkyLand Key Bypass
 color d
-echo Para obter seu HWID, clique no botão "Obter chave" no aplicativo e copie os dados da Pesquisa &echo.
+echo Para obter seu HWID, clique no botao "Obter chave" no aplicativo e copie os dados da Pesquisa &echo.
 echo Tenha em mente que cada HWID tem sua propria chave &echo.
 echo Exemplo: https://flux.li/windows/start.php?HWID=XXXX (Copie Tudo Que Aparecer No Lugar De XXX) &echo. &echo.
 set /p hwid=Coloque Seu HWID: 
